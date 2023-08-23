6fd91f6a6a0cd8efce773757c3a33ce8d18be077
Anastasia <ms.anastasia.bogdanova@gmail.com>
2023-08-23 19:29:14 +0300

    add image picker for cocktail imageView

--- app/src/main/java/com/example/cocktail_bar/CreateCocktailsFragment.kt
+++ app/src/main/java/com/example/cocktail_bar/CreateCocktailsFragment.kt
@@ -1,11 +1,21 @@
 package com.example.cocktail_bar
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Audio.Media
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.cardview.widget.CardView
 
 class CreateCocktailsFragment : Fragment() {
 
@@ -21,13 +31,41 @@ class CreateCocktailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val cancelButton: Button = view.findViewById(R.id.buttonCancel)
+        val cardViewImageCocktail: CardView = view.findViewById(R.id.cardViewImageCocktail)
 
         cancelButton.setOnClickListener{
             requireActivity().supportFragmentManager.popBackStack()
         }
+
+        cardViewImageCocktail.setOnClickListener{
+            openGallery()
+        }
     }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImageUri = data.data
+            loadSelectedImage(selectedImageUri)
+        }
+    }
+
+    private fun loadSelectedImage(imageUri: Uri?) {
+        val imageView = view?.findViewById<ImageView>(R.id.imageCocktail)
+        val inputStream = imageUri?.let { requireActivity().contentResolver.openInputStream(it) }
+        val bitmap = BitmapFactory.decodeStream(inputStream)
+        imageView?.setImageBitmap(bitmap)
+    }
+
     companion object {
         @JvmStatic
         fun newInstance() = CreateCocktailsFragment()
+        private const val GALLERY_REQUEST_CODE = 123
     }
 }
